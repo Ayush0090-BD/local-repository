44761ca39ef0386f3593557545570c3b21c12ac2
Ayush0090-BD <ajaypa123k@gmail.com>
2025-10-18 23:44:47 +0530

    swap

---
+++ Ayush.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Ayush
+{
+   public static void main(String[] args)
+   {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int S=sc.swap(a,b);
+       System.out.println(S);
+   }
+   int Swap(a.,b)
+   {
+       int temp;
+       a=temp;
+       b=a;
+       temp=b;
+
+   }
+}
